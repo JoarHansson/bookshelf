28ca44f4a2affc7b51058773095facecd8067cf0
JoarHansson <124060119+JoarHansson@users.noreply.github.com>
2023-10-23 13:54:05 +0200

    set up files, print "a bookshelf" in the browser

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+require __DIR__ . "/header.php";
+require __DIR__ . "/data.php";
+
+?>
+
+<div class="bookshelf">
+  <div class="shelves">
+    <div class="shelf"></div>
+    <div class="shelf"></div>
+    <div class="shelf"></div>
+    <div class="shelf"></div>
+    <div class="shelf"></div>
+  </div>
+  <div class="legs">
+    <div class="leg"></div>
+    <div class="leg"></div>
+  </div>
+</div>
+
+</body>
+
+</html>
