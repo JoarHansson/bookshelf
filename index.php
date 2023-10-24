20aa8b4cd03d4e94bcea5055b0e93fbd9e630a0d
JoarHansson <124060119+JoarHansson@users.noreply.github.com>
2023-10-24 15:16:30 +0200

    add sorting functionality + change some styles

--- index.php
+++ index.php
@@ -5,16 +5,25 @@ declare(strict_types=1);
 require __DIR__ . "/header.php";
 require __DIR__ . "/data.php";
 
+if (isset($_GET["sort"])) :
+  $sortBy = $_GET["sort"]; ?>
+  <p><?= $sortBy; ?></p>
+<?php endif;
+
+$sortingKey = array_column($books, $sortBy);
+array_multisort($sortingKey, SORT_ASC, $books);
+
 ?>
 
 <div class="bookshelf">
-  <form action="index.php" method="get">
+  <form action="index.php" method="get" class="form-bookshelf">
     <?php foreach ($books as $key => $book) : ?>
+
       <button type="submit" value="<?= $book["id"] ?>" name="id" class="book" style="
         background-color: <?= $book["color"] ?>;
-        width: <?= $book["pages"] / 4 ?>px;
-      ">
-        <p class="rotate" style="transform: rotate(90deg);">test</p>
+        width: <?= $book["pages"] / 4 ?>px;">
+        <p><?= $book["title"] ?></p>
+        <p><?= $book["author"] ?></p>
       </button>
 
     <?php endforeach ?>
@@ -28,17 +37,35 @@ require __DIR__ . "/data.php";
 
 
 <div class="selection">
-  <?php
-  if (isset($_GET["id"])) :
-    $selection = $_GET["id"] ?>
-    <p>Title: <?= $books[$selection]["title"]; ?></p>
-    <p>Author: <?= $books[$selection]["author"]; ?></p>
-    <p>Genre: <?= $books[$selection]["genre"]; ?></p>
-    <p>Release year: <?= $books[$selection]["year"]; ?></p>
-    <p>Number of pages: <?= $books[$selection]["pages"]; ?></p>
-
+  <?php if (isset($_GET["id"])) :
+    $selectedBook = $_GET["id"]; ?>
+    <p>Title: <?= $books[$selectedBook]["title"]; ?></p>
+    <p>Author: <?= $books[$selectedBook]["author"]; ?></p>
+    <p>Genre: <?= $books[$selectedBook]["genre"]; ?></p>
+    <p>Release year: <?= $books[$selectedBook]["year"]; ?></p>
+    <p>Number of pages: <?= $books[$selectedBook]["pages"]; ?></p>
   <?php endif; ?>
 </div>
+
+<div class="sort">
+  <form action="index.php" method="get">
+    <label for="sort">sort by:</label>
+    <select id="sort" name="sort">
+      <option value="title" selected>Title</option>
+      <option value="author">Author</option>
+      <option value="pages">Pages</option>
+      <option value="year">Release year</option>
+      <option value="color">Color of book</option>
+    </select>
+    <button type="submit">OK</button>
+  </form>
+</div>
+
+<?php
+
+echo "<pre>";
+var_export($books);
+?>
 
 </body>
 
